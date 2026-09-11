b9b57a679b751e519015223b651f501456e0542c
markvoronin354 <markvoronin354@gmail.com>
2026-09-10 21:00:43 -0400

    v1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.markvoronin.reelsonthego"
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
